4e9eedcc2be6f9abb07d842208b2d222b39fb273
Abhijat Malviya <malviya.abhijat@gmail.com>
2017-09-10 22:09:36 +0530

    configure the rabbitmq components

--- src/main/java/am/complaints/AppConfig.java
+++ src/main/java/am/complaints/AppConfig.java
@@ -2,13 +2,42 @@ package am.complaints;
 
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Value("${axon.amqp.exchange}")
+    private String address;
+
     @Bean
     public Serializer serializer() {
         return new JacksonSerializer();
+    }
+
+    @Bean
+    public Exchange exchange() {
+        return ExchangeBuilder.fanoutExchange(address).build();
+    }
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable(address).build();
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with("*").noargs();
+    }
+
+    @Autowired
+    public void configure(AmqpAdmin amqpAdmin) {
+        amqpAdmin.declareExchange(exchange());
+        amqpAdmin.declareQueue(queue());
+        amqpAdmin.declareBinding(binding());
     }
 }
