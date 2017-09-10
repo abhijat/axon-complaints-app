651e0389d430d122f5af377195e2e925f9efaad2
Abhijat Malviya <malviya.abhijat@gmail.com>
2017-09-10 21:20:57 +0530

    remove repository name, not required

--- src/main/java/am/complaints/ComplaintQueryObjectRepository.java
+++ src/main/java/am/complaints/ComplaintQueryObjectRepository.java
@@ -3,6 +3,6 @@ package am.complaints;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository("complaint_query_store")
+@Repository
 public interface ComplaintQueryObjectRepository extends JpaRepository<ComplaintQueryObject, String> {
 }
